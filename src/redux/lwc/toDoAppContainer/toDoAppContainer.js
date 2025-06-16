736d6183911cc7d53ebdae1362a7278b9b936a3f
brooksjohnson <brooksjohnson@bostonbeer.com>
2025-06-16 15:14:49 -0400

    set up basic structure

--- src/redux/lwc/toDoAppContainer/toDoAppContainer.js
+++ src/redux/lwc/toDoAppContainer/toDoAppContainer.js
@@ -3,7 +3,27 @@
  */
 
 import {LightningElement} from 'lwc';
+import { createStore, combineReducers, createLogger } from "c/lwcRedux";
+import reducers from "c/todoAppReducer"
+
+let ENABLE_LOGGING = true;
 
 export default class ToDoAppContainer extends LightningElement {
+
+    @api store;
+
+    initialize(){
+        let logger;
+
+        if(ENABLE_LOGGING){
+            logger = createLogger({
+                duration: true,
+                diff: true,
+            })
+        }
+        const combineReducersInstance = combineReducers(reducers);
+        // eslint-disable-next-line @lwc/lwc/no-api-reassignments
+        this.store = createStore(combineReducersInstance, logger)
+    }
 
 }
---
+++ src/redux/lwc/todoAppReducer/todoAppReducer.js
@@ -0,0 +1,9 @@
+/**
+ * Created by brooksjohnson on 6/16/25.
+ */
+
+class TodoAppReducer {
+
+}
+
+export {TodoAppReducer}
